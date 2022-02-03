970ec0aa09b0e64c833bab575dfc4cec89155813
elephantMax <hatypovm12@mail.ru>
2022-02-03 17:24:23 +0300

    init commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="app">
+    <button class="app__btn">Test</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {},
+}
+</script>
+
+<style lang="scss" scoped>
+.app {
+  &__btn {
+    transform: rotate(90deg);
+  }
+}
+</style>
